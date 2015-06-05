fd63e62eeffcd6192cb1d38d64e0f6dced0ad214
alimhossen <ahosssenstatru@gmail.com>
2015-06-05 10:35:15 +0600

    package initialization

---
+++ R/meanx.R
@@ -0,0 +1,8 @@
+meanx <-
+function(x)
+{
+ n <-length(x)
+ sumx <-sum(x)
+ avgx <-sumx/n
+ return(avgx)
+}
---
+++ R/meanx2.R
@@ -0,0 +1,13 @@
+meanx2 <-
+function(x)
+{
+ sumx=0
+ 
+ for(i in 1:length(x))
+  {
+    sumx <- sumx+x[i]
+  }
+
+ avgx <-sumx/length(x)
+ return(avgx)
+}
